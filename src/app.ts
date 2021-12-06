a32a48ffb1e0555d69dc574c02f59153a41b1719
Lukas Vögl <lukas@voegl.org>
2021-12-06 12:08:06 +0100

    add code documentation
    Signed-off-by: Lukas Vögl <lukas@voegl.org>

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
+/**
+ * Sums up all given parameters
+ * @param summands numbers to sum up
+ * @returns the total sum
+ */
 const sum = (...summands: number[]): number => {
     return summands.reduce((summand, total) => total + summand);
 };
 
+/**
+ * Multiplies all given parameters
+ * @param factors numbers to multiply
+ * @returns the product of all numbers
+ */
 const multiply = (...factors: number[]): number => {
     return factors.reduce((factor, total) => total * factor);
 };
